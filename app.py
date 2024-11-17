a1a05bfa5ef271bbf6c7e902b206b1d3a837c07a
ShreeNithiB <shreezz2411@gmail.com>
2024-11-16 18:54:25 -0800

    details added

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ st.write("hello welcome to my project")
 
 name=st.text_input("enter your name")
 st.write(f"welcome {name}")
+st.write("welcome sir")
+st.write("I have completed my schooling in coimbatore")
+st.write("In this resume I have described about myself")
 btn =st.button("click me")
 if btn:
     st.write("hi how are you")
@@ -15,3 +18,4 @@ st.dataframe(Dict)
 skills=["python","java"]
 for skill in skills:
     st.write(f"-{skill}")    
+    
